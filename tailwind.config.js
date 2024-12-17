83da5691aaee4ae87e58926c0e2022da06e1df06
malikovshahriyor <malikovshahriyor929@gmail.com>
2024-12-17 09:54:36 +0500

    s errb

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily:{
+sans :"Manrope"
+      },
+      colors:{
+        primary :"#5b5729",
+        primarygreen:"#3e4d31",
+      }
+    },
+  },
+  plugins: [],
+}
